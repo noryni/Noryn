f3784cb9bae84a30a7ae115fe93217dc00a9dc62
Nory <amiroamiro0981@gmail.com>
2026-05-26 03:47:02 +0400

    Update index.js

--- Assistance/Status/index.js
+++ Assistance/Status/index.js
@@ -14,7 +14,7 @@ Noryn_App.listen(Noryn_Port, () => {
   console.log(`[Noryn] Website online on port ${Noryn_Port}`);
 });
 const Noryn_Status_List = [
- 'Join today or stay forgotten.',
+  'Join today or stay forgotten.',
   'http://dsc.gg/getnoryn'
 ];
 let Noryn_Status_Index = 0;
@@ -35,7 +35,7 @@ function Noryn_Update_Status() {
 async function Noryn_Login() {
   try {
     await Noryn_Client.login(process.env.Key);
-    Noryn_Update_Status();
+    Noryn_Update_Status(); 
   } catch (err) {
     console.error('[Noryn] Login failed:', err);
   }
